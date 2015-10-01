945b8b0a6d48fd350dd4d787c3a7328ce8945111
ramshresh <sendmail4ram@gmail.com>
2015-10-02 02:28:32 +0545

    bulk

--- _protected/api/controllers/MonumentController.php
+++ _protected/api/controllers/MonumentController.php
@@ -8,7 +8,9 @@
 
 namespace api\controllers;
 
-
+use Yii;
+use yii\helpers\Json;
+use yii\log\Logger;
 use yii\rest\Controller;
 
 class MonumentController extends Controller
@@ -16,6 +18,8 @@ class MonumentController extends Controller
     public $modelClass = 'common\models\User';
 
     public function actionIndex(){
+
+        Yii::trace('Recieved data ' . Json::encode(Yii::$app->request->post()));
         return ['status'=>'success','msg'=>'ok','object'=>[]];
     }
 
