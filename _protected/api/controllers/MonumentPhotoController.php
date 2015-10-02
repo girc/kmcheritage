4a88701b3d11d2790192435c313888684e6856a6
ramshresh <sendmail4ram@gmail.com>
2015-10-02 08:23:20 +0545

    bulk

--- _protected/api/controllers/MonumentPhotoController.php
+++ _protected/api/controllers/MonumentPhotoController.php
@@ -9,9 +9,12 @@
 namespace api\controllers;
 
 
+use common\models\MonumentPhoto;
 use yii\helpers\Json;
 use yii\rest\Controller;
 use Yii;
+use yii\web\UploadedFile;
+
 class MonumentPhotoController extends Controller
 {
     public $modelClass = 'common\models\MonumentPhoto';
@@ -23,6 +26,33 @@ class MonumentPhotoController extends Controller
 
     public function actionCreate(){
 
-        return ['status'=>'success','msg'=>'ok','object'=>Yii::$app->request->post()];
+        $model = new MonumentPhoto();
+
+        /*if ($model->load(Yii::$app->request->post())) {
+            $transaction = Yii::$app->db->beginTransaction();
+            if ($model->save()) {
+                if (UploadedFile::getInstance($model, 'image')) {
+                    // process uploaded image file instance
+                    $image = UploadedFile::getInstance($model, 'image');
+                    // upload only if valid uploaded file instance found
+                    if ($image !== false) {
+                        try {
+                            $model->saveUploadedImage($image);
+                        } catch (Exception $e) {
+                            $transaction->rollBack();
+                        }
+                    }
+                }
+                $transaction->commit();
+                return $this->redirect(['view', 'id' => $model->id]);
+            } else {
+                // error in saving model
+            }
+        }
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+*/
+        return ['status'=>'success','msg'=>'ok','object'=>UploadedFile::getInstance($model,'image_file')];
     }
 }
